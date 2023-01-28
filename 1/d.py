6928d9cf416e1b7007b8581db24ce6d10196e782
Dokaaa <122877634+Dokaaa@users.noreply.github.com>
2023-01-29 03:13:51 +0600

    Delete 1 directory

--- 1/d.py
+++
@@ -1,6 +0,0 @@
-first = 5
-second = 7
-print(first * second)
-first = '5'
-second = '7'
-print(int(first) * int(second))
--- 1/zadachi/8.py
+++
@@ -1,6 +0,0 @@
-a=int(input())
-b=int(input())
-l=int(input())
-N=int(input())
-x = 2 * l + a * (2*N-1) + 2 * b * (N-1)
-print(x)
